6fc80a13daa1850e4b4639e493bda2cb1508e069
ctangarife <cgt9145@gmail.com>
2022-07-16 10:19:33 -0500

    Se vuelve a generar migracion para correcion de relaciones

--- src/migrations/1657983691944-model_create.ts
+++ src/migrations/1657983691944-model_create.ts
@@ -1,30 +1,35 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class modelOrganization1657939312324 implements MigrationInterface {
-  name = 'modelOrganization1657939312324';
+export class modelCreate1657983691944 implements MigrationInterface {
+  name = 'modelCreate1657983691944';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `CREATE SEQUENCE IF NOT EXISTS "organization_id_seq"`,
     );
     await queryRunner.query(
-      `CREATE TABLE IF NOT EXISTS "organization" ("created_at" timestamp NOT NULL DEFAULT current_timestamp(), "updated_at" timestamp NOT NULL DEFAULT current_timestamp(), "id" INT DEFAULT nextval('"organization_id_seq"') NOT NULL, "name" varchar(500) NOT NULL, "status" int8 NOT NULL, CONSTRAINT "PK_472c1f99a32def1b0abb219cd67" PRIMARY KEY ("id"))`,
+      `CREATE TABLE IF NOT EXISTS "organization" ("created_at" timestamp NOT NULL DEFAULT current_timestamp(), "updated_at" timestamp NOT NULL DEFAULT current_timestamp(), "id" INT DEFAULT nextval('"organization_id_seq"') NOT NULL, "name" varchar(50) NOT NULL, "status" int8 NOT NULL, CONSTRAINT "PK_472c1f99a32def1b0abb219cd67" PRIMARY KEY ("id"))`,
     );
-    await queryRunner.query(`CREATE SEQUENCE "tribe_id_seq"`);
+    await queryRunner.query(`CREATE SEQUENCE IF NOT EXISTS "tribe_id_seq"`);
     await queryRunner.query(
       `CREATE TABLE IF NOT EXISTS "tribe" ("created_at" timestamp NOT NULL DEFAULT current_timestamp(), "updated_at" timestamp NOT NULL DEFAULT current_timestamp(), "id" INT DEFAULT nextval('"tribe_id_seq"') NOT NULL, "name" varchar(50) NOT NULL, "status" int8 NOT NULL, "id_organization" int8, CONSTRAINT "PK_43066292406f7b89ee83eb70c69" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `CREATE INDEX IF NOT EXISTS "IDX_dcdd03e44f28bdf784d4331482" ON "tribe" ("id_organization") `,
     );
-    await queryRunner.query(`CREATE SEQUENCE "repository_id_seq"`);
     await queryRunner.query(
-      `CREATE TABLE IF NOT EXISTS "repository" ("created_at" timestamp NOT NULL DEFAULT current_timestamp(), "updated_at" timestamp NOT NULL DEFAULT current_timestamp(), "id" INT DEFAULT nextval('"repository_id_seq"') NOT NULL, "name" varchar(50) NOT NULL, "state" varchar(1) NOT NULL DEFAULT 'E', "status" varchar(1) NOT NULL DEFAULT 'A', "id_tribe" int8, CONSTRAINT "PK_b842c26651c6fc0b9ccd1c530e2" PRIMARY KEY ("id"))`,
+      `CREATE SEQUENCE IF NOT EXISTS "repository_id_seq"`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE IF NOT EXISTS "repository" ("created_at" timestamp NOT NULL DEFAULT current_timestamp(), "updated_at" timestamp NOT NULL DEFAULT current_timestamp(), "id" INT DEFAULT nextval('"repository_id_seq"') NOT NULL, "name" varchar(50) NOT NULL, "state" varchar(1) NOT NULL DEFAULT 'E', "status" varchar(1) NOT NULL DEFAULT 'A', "id_tribe" int8, "metricId" int8, CONSTRAINT "REL_9b054508d0a508525d9918b397" UNIQUE ("metricId"), CONSTRAINT "PK_b842c26651c6fc0b9ccd1c530e2" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `CREATE INDEX IF NOT EXISTS "IDX_3572e75b71052040481c022dfd" ON "repository" ("id_tribe") `,
     );
-    await queryRunner.query(`CREATE SEQUENCE "metrics_id_seq"`);
+    await queryRunner.query(
+      `CREATE INDEX IF NOT EXISTS "IDX_9b054508d0a508525d9918b397" ON "repository" ("metricId") `,
+    );
+    await queryRunner.query(`CREATE SEQUENCE IF NOT EXISTS "metrics_id_seq"`);
     await queryRunner.query(
       `CREATE TABLE IF NOT EXISTS "metrics" ("created_at" timestamp NOT NULL DEFAULT current_timestamp(), "updated_at" timestamp NOT NULL DEFAULT current_timestamp(), "id" INT DEFAULT nextval('"metrics_id_seq"') NOT NULL, "coverage" float8 NOT NULL, "bugs" int8 NOT NULL, "vulnerabilities" int8 NOT NULL, "hotspot" int8 NOT NULL, "code_smells" int8 NOT NULL, "id_repository" int8, CONSTRAINT "REL_c3d911b1d911a990e617041947" UNIQUE ("id_repository"), CONSTRAINT "PK_5283cad666a83376e28a715bf0e" PRIMARY KEY ("id"))`,
     );
@@ -38,6 +43,9 @@ export class modelOrganization1657939312324 implements MigrationInterface {
       `ALTER TABLE "repository" ADD CONSTRAINT "FK_3572e75b71052040481c022dfdd" FOREIGN KEY ("id_tribe") REFERENCES "tribe"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
     await queryRunner.query(
+      `ALTER TABLE "repository" ADD CONSTRAINT "FK_9b054508d0a508525d9918b3974" FOREIGN KEY ("metricId") REFERENCES "metrics"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
       `ALTER TABLE "metrics" ADD CONSTRAINT "FK_c3d911b1d911a990e617041947b" FOREIGN KEY ("id_repository") REFERENCES "repository"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
   }
@@ -45,6 +53,9 @@ export class modelOrganization1657939312324 implements MigrationInterface {
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE "metrics" DROP CONSTRAINT "FK_c3d911b1d911a990e617041947b"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "repository" DROP CONSTRAINT "FK_9b054508d0a508525d9918b3974"`,
     );
     await queryRunner.query(
       `ALTER TABLE "repository" DROP CONSTRAINT "FK_3572e75b71052040481c022dfdd"`,
@@ -57,6 +68,9 @@ export class modelOrganization1657939312324 implements MigrationInterface {
     );
     await queryRunner.query(`DROP TABLE "metrics"`);
     await queryRunner.query(`DROP SEQUENCE "metrics_id_seq"`);
+    await queryRunner.query(
+      `DROP INDEX "repository"@"IDX_9b054508d0a508525d9918b397" CASCADE`,
+    );
     await queryRunner.query(
       `DROP INDEX "repository"@"IDX_3572e75b71052040481c022dfd" CASCADE`,
     );
